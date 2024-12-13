6064147ba4801a8930cc486d52c129b1836a3b19
CarlosLV555 <carlosiglo99@gmail.com>
2024-12-13 13:36:06 +0100

    Add files via upload

---
+++ data/Shuttle_4.js
@@ -0,0 +1,1 @@
+var json_Shuttle_4 = {"type":"FeatureCollection","name":"Shuttle_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[7.03266003909652,49.34336361012825]}}]}
